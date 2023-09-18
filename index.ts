2b1070b60c4636efd4f1568fdf36ec71d7aa18f7
exhq <infidelLOLer@gmail.com>
2023-09-18 18:59:58 +0330

    first commit

---
+++ index.ts
@@ -0,0 +1,49 @@
+import { createCors, error, json, RequestLike, Router } from 'itty-router'
+import token from "./config.json"
+
+const baseurl = 'https://discord.com/api/v10/users/'
+const { preflight, corsify } = createCors({
+    methods: ['GET', 'PATCH', 'POST'],
+    origins: ['http://localhost:3001'],
+})
+
+const router = Router()
+
+function fetchData(id:string) {
+    const url = baseurl + id;
+    const headers = {
+        "Authorization": `Bot ${token.token}`,
+    };
+
+    return fetch(url, {
+        method: 'GET',
+        headers: headers,
+    })
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    });
+}
+
+router
+    .all('*', preflight)
+    .get('/v1/:id', (handler) => {
+            return fetchData(handler.params.id)
+    })
+export default {
+    port: 3001,
+    fetch: (request: RequestLike) => router
+        .handle(request)
+
+        // transform unformed responses
+        .then(json)
+
+        // catch any errors
+        .catch(error)
+
+        // add CORS headers to all requests,
+        // including errors
+        .then(corsify)
+}
